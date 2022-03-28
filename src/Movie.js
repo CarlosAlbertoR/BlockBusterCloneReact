94eba8ebec35d9f384cc3285f33573baf47772c0
Carlos Alberto R <carlosalbertorios3@gmail.com>
2022-03-28 01:25:42 -0500

    Creation of Movie component, customization of App.js and generation of JSON file for movie consumption

---
+++ src/Movie.js
@@ -0,0 +1,44 @@
+function Movie(props) {
+    const listDirectors = props.directors.split(",").map((dir, i) => {
+        return (
+            i !== props.directors.split(",").length - 1
+                ? <a href="#">{dir},</a>
+                : <a href="#">{dir}</a>
+        )
+    })
+
+    const listActors = props.actors.split(",").map((act, i) => {
+        return (
+            i !== props.actors.split(",").length - 1
+                ? <a href="#">{act},</a>
+                : <a href="#">{act}</a>
+        )
+    })
+
+    return (
+        <div className="movie-item-style-2">
+            <img src={props.coverImage} alt="" />
+            <div className="mv-item-infor">
+                <h6>
+                    <a href="moviesingle.html">{props.title} <span>({props.year})</span></a>
+                </h6>
+                <p className="rate">
+                    <i className="ion-android-star"></i><span>{props.rate}</span> /10
+                </p>
+                <p className="describe">{props.children}</p>
+                <p className="run-time">
+                    Run Time: {props.runTime} . <span>MMPA: {props.mmpa} </span> .
+                    <span>Release: {props.release}</span>
+                </p>
+                <p>Director: {listDirectors}</p>
+
+                <p>
+                    Stars:
+                    {listActors}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Movie;
