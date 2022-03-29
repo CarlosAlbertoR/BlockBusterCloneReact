010448286fd82725d8c28542f50334c7b8e850b8
Carlos Alberto R <carlosalbertorios3@gmail.com>
2022-03-29 01:45:55 -0500

    Adicion de prettier y eslint

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import { useState } from 'react';
+import { React, useState } from 'react';
 import './App.css';
 import PageWrapper from './PageWrapper';
 import Movie from './Movie';
 import Pagination from './Pagination';
-import moviesJson from './movies.json'
+import moviesJson from './movies.json';
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [moviesPerPage, setMoviesPerPage] = useState(5);
-  
-  let movies = moviesJson;
 
-  const loadMovies = () => {
-    return (
-      movies.slice(
-        (currentPage - 1) * moviesPerPage,
-        (currentPage - 1) * moviesPerPage + moviesPerPage
-      )
+  const movies = moviesJson;
+
+  // const searchMovies = async () => {
+  //   const url = 'https://data-develop-carlos.s3.amazonaws.com/movies.json';
+  //   movies = await fetch(url, {
+  //     method: 'GET',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json'
+  //     }
+  //   });
+  //   console.log('movies', movies);
+  // };
+
+  const loadMovies = () =>
+    movies.slice(
+      (currentPage - 1) * moviesPerPage,
+      (currentPage - 1) * moviesPerPage + moviesPerPage
     );
-  }
 
-
-  const getTotalPages = () => {
-    return Math.ceil(moviesJson.length / moviesPerPage);
-  }
+  const getTotalPages = () => Math.ceil(moviesJson.length / moviesPerPage);
 
   return (
     <PageWrapper>
-      {loadMovies().map(movie =>
+      {loadMovies().map((movie) => (
         <Movie
           title={movie.title}
           year={movie.year}
@@ -41,13 +47,13 @@ function App() {
         >
           {movie.description}
         </Movie>
-      )}
+      ))}
 
       <Pagination
         current={currentPage}
         total={getTotalPages()}
         onChangePage={(page) => {
-          setCurrentPage(page)
+          setCurrentPage(page);
         }}
         onChangeItemsPerPage={(newValue) => {
           setMoviesPerPage(newValue);
